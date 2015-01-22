bdc9cbacc02db40e50bd77018da259fbe727abcf
Ammar Alakkad <am.alakkad@gmail.com>
2015-01-22 10:57:47 +0200

    Use bloginfo() to display the site name isntead

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
             <nav class="top-bar" data-topbar role="navigation" data-options="custom_back_text: true; back_text: عودة">
                 <ul class="title-area">
                     <li class="name">
-                        <h1><a href="<?= home_url();?>">أخبار المالية العامة</a></h1>
+                        <h1><a href="<?= home_url();?>"><?php bloginfo('name');?></a></h1>
                     </li>
                     <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
                 </ul>
--- partials/header-sharebar.php
+++ partials/header-sharebar.php
@@ -3,7 +3,7 @@
         <nav class="top-bar" data-topbar="">
             <ul class="title-area">
                 <li class="name">
-                    <h1><a href="<?= home_url();?>">أخبار المالية العامة</a></h1>
+                    <h1><a href="<?= home_url();?>"><?php bloginfo('name');?></a></h1>
                 </li>
                 <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
             </ul>
