af71b8336b98d438baa7d1bc67d3e638da095f7d
Ammar Alakkad <am.alakkad@gmail.com>
2015-01-22 11:02:22 +0200

    Header: remove empty meta description tag

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html class="no-js" lang="ar" dir="rtl">
 <head>
     <meta charset="utf-8">
-    <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
     <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
     <?php wp_head();?>
